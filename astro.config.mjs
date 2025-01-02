89ce79b7bffd88e828d11eca5757ee3a46c4da8e
Xantibody <zeku.bushinryu38@gmail.com>
2025-01-02 21:47:13 +0900

    fix domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://raizawa-blog.pages.dev',
     integrations: [mdx(), sitemap(), react()],
 });
